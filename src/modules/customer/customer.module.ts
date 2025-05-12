6990a39df800bfeb6235504400a064d0609a3b79
baoIsHereeee <hallotolastewie@gmail.com>
2025-05-12 15:55:15 +0700

    Small Fix in Customer Module

--- src/modules/customer/customer.module.ts
+++ src/modules/customer/customer.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CustomerService } from './services/customer.service';
 import CustomerRepository from './repositories/customer.repository';
+import { CustomerController } from './customer.controller';
 
 @Module({
     providers: [CustomerService, CustomerRepository],
-    exports: [CustomerRepository]
+    exports: [CustomerRepository],
+    controllers: [CustomerController]
 })
 export class CustomerModule {}
